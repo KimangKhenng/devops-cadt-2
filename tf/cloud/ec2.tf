b370e3346a59d1f156f5abef0310f017dfbca09c
KimangMac14 <kimangkheang@gmail.com>
2024-12-18 15:31:48 +0700

    AWS Prod

--- tf/cloud/ec2.tf
+++ tf/cloud/ec2.tf
@@ -48,32 +48,8 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "server_1" {
   ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t3.micro"
+  instance_type               = "t3.xlarge"
   key_name                    = aws_key_pair.kkk3_key.key_name
   security_groups             = [aws_security_group.sg_1.name]
-  user_data                   = <<-EOF
-              #!/bin/bash
-              apt update
-              apt install git -y
-              apt install curl -y
-
-              # Install NVM
-              curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-              . ~/.nvm/nvm.sh
-
-              # Install Node.js 18
-              nvm install 18
-
-              # Install PM2
-              npm install pm2 -g
-
-              # Clone Node.js repository
-              git clone https://github.com/KimangKhenng/devops-ex /root/devops-ex
-
-              # Navigate to the repository and start the app with PM2
-              cd /root/devops-ex
-              npm install
-              pm2 start app.js --name node-app -- -p 8000
-            EOF
   user_data_replace_on_change = true
 }
--- tf/local/output.tf
+++ tf/local/output.tf
@@ -1,4 +1,4 @@
-output "ec2ipv4_1" {
+output "ec2ipv4" {
   value       = aws_instance.server_1.public_ip
   description = "Ipv4"
 }
