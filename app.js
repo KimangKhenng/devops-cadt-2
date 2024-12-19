437655586e8c4a71ecf128e9193d10c1c8262ee8
KimangMac14 <kimangkheang@gmail.com>
2024-12-19 21:46:22 +0700

    Update index

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello World 你好');
+    res.status(200).send('Hello World សួស្តីពីការកែទំរង់');
 });
 
 app.get('/user/:id', (req, res) => {
